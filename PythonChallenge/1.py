9c6eff3e82fe173c7fac112943d48c8d3ff40e66
Derek <djones@doba.com>
2016-09-27 14:20:03 -0600

    challenge 1 complete

--- PythonChallenge/1.py
+++ PythonChallenge/1.py
@@ -1,2 +1,16 @@
 #!/bin/python
-#
+#http://www.pythonchallenge.com/pc/def/map.html
+
+from string import maketrans
+
+scrambled_phrase = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+alphabet="abcdefghijklmnopqrstuvwxyz"
+key=""
+for letter in alphabet:
+	key+=chr((ord(letter)-17)%26+97)
+table = maketrans(alphabet, key)
+
+print scrambled_phrase.translate(table)+"\n"
+
+print "URL= http://www.pythonchallenge.com/pc/def/"+"map".translate(table)+".html"
