a05a77efd5e0428fcd78ec1a4d6c01c32002261a
derek.jones@source.vivint.com <derek.jones562@gmail.com>
2019-05-17 13:37:56 -0600

    update GameofLife for classes

--- GameOfLife/life.py
+++ GameOfLife/life.py
@@ -6,88 +6,95 @@ from subprocess import call
 
 # https://robertheaton.com/2018/07/20/project-2-game-of-life/
 
-def create_row(length):
-    row = []
-    for i in range(length):
-        row.append(0)
-    return row
+class Board(object):
 
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = self.random_state()
 
-def dead_state(width, height):
-    board = []
-    for i in range(height):
-        board.append(create_row(width))
-    return board
+    def __str__(self):
+        board_as_string = ""
+        call('clear' if os.name == 'posix' else 'cls')
+        for column in range(self.width):
+            board_as_string += "---"
+        board_as_string += "---\n"
+        for row in range(self.height):
+            board_as_string += "|"
+            for column in range(self.width):
+                if self.board[row][column] == 1:
+                    board_as_string += " # "
+                else:
+                    board_as_string += "   "
+            board_as_string += "|\n"
+        for column in range(self.width):
+            board_as_string += "---"
+        board_as_string += "---\n"
+        return board_as_string
 
+    def __repr__(self):
+        return self.__str__()
 
-def random_state(width, height):
-    board = dead_state(width, height)
-    for row in range(len(board)):
-        for column in range(len(board[row])):
-            random_number = random.random()
-            if random_number <= .5:
-                board[row][column] = 1
-    return board
+    def _create_dead_row(self):
+        row = []
+        for i in range(self.width):
+            row.append(0)
+        return row
 
+    def _dead_state(self):
+        dead_board = []
+        for i in range(self.height):
+            dead_board.append(self._create_dead_row())
+        return dead_board
 
-def print_board(board):
-    call('clear' if os.name =='posix' else 'cls')
-    for column in range(len(board[0])):
-        print("---", end='')
-    print("---")
-    for row in range(len(board)):
-        print("|", end='')
-        for column in range(len(board[row])):
-            if board[row][column] == 1:
-                print(" # ", end='')
-            else:
-                print("   ", end='')
-        print("|")
-    for column in range(len(board[0])):
-        print("---", end='')
-    print("---")
+    def random_state(self):
+        random_board = self._dead_state()
+        for row in range(self.height):
+            for column in range(self.width):
+                random_number = random.random()
+                if random_number <= .5:
+                    random_board[row][column] = 1
+        return random_board
 
+    def _calculate_number_of_neighbors(self, location):
+        neighbors = 0
+        for i in range(-1, 2):
+            x = location[0] + i
+            for j in range(-1, 2):
+                y = location[1] + j
+                # print("({},{}):{}".format(x, y, board[x][y]))
+                if x == location[0] and y == location[1]:
+                    pass
+                elif x >= 0 and y >= 0 and y < self.width and x < self.height:
+                    if self.board[x][y] == 1:
+                        neighbors += 1
+        return neighbors
 
-def calculate_number_of_neighbors(board, location):
-    neighbors = 0
-    for i in range(-1, 2):
-        x = location[0] + i
-        for j in range(-1, 2):
-            y = location[1] + j
-            # print("({},{}):{}".format(x, y, board[x][y]))
-            if x == location[0] and y == location[1]:
-                pass
-            elif x >= 0 and y >= 0 and y < len(board[0]) and x < len(board):
-                if board[x][y] == 1:
-                    neighbors += 1
-    return neighbors
+    def next_board_state(self):
+        new_board = self._dead_state()
+        for row in range(self.height):
+            for column in range(self.width):
+                neighbors = self._calculate_number_of_neighbors((row, column))
+                alive = self.board[row][column]
+                if alive:
+                    rules = {2: 1,
+                             3: 1}
 
+                else:
+                    rules = {3: 1}
+                new_board[row][column] = rules.get(neighbors, 0)
+        return new_board
 
-def next_board_state(board):
-    new_board = dead_state(len(board[0]), len(board))
-    for row in range(len(board)):
-        for column in range(len(board[row])):
-            neighbors = calculate_number_of_neighbors(board, (row, column))
-            alive = board[row][column]
-            if alive:
-                rules = {2: 1,
-                         3: 1}
+    def is_stable(self):
+        return False
 
-            else:
-                rules = {3: 1}
-            new_board[row][column] = rules.get(neighbors, 0)
-    return new_board
-
-
-def play_game():
-    board = random_state(50, 35)
-    while True:
-        print_board(board)
-        new_board = next_board_state(board)
-        if new_board == board:
-            break
-        else:
-            board = new_board
+    def play_game(self):
+        print(self.__str__())
+        new_board = self.next_board_state()
+        self.board = new_board
         time.sleep(.15)
 
-play_game()
+
+board = Board(5, 5)
+while not board.is_stable():
+    board.play_game()
