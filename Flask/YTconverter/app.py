ed21526adf39e02fbbb6bba894eb686e34bed3b7
derekjones562 <derek.jones562@gmail.com>
2024-02-16 00:07:40 -0800

    convert to yt-dlp

--- Flask/YTconverter/app.py
+++ Flask/YTconverter/app.py
@@ -1,10 +1,18 @@
 #!python3
-from flask import Flask, render_template, redirect, url_for, request
+import time
 
-from pytube import YouTube
-#from flask_socketio import SocketIO
+from flask import Flask, render_template, redirect, url_for, request, send_file, Response
+
+import yt_dlp
+import logging
+
+from filenamePostProcessor import FilenameCollectorPP
+
+logger = logging.getLogger(__name__)
+
+# from flask_socketio import SocketIO
 app = Flask(__name__)
-#socketio = SocketIO(app)
+# socketio = SocketIO(app)
 
 
 @app.route('/')
@@ -12,15 +20,52 @@ def index():
     return render_template('yt_converter.html', **request.args)
 
 
+@app.route('/download_mp4/', methods=['GET'])
+def redirect_after_download():
+    return redirect("/")
+
+
 @app.route('/download_mp4/', methods=['POST'])
 def download_mp4():
+    def generate():
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.add_post_processor(filename_collector)
+            error_code = ydl.download(url)
+            if error_code:
+                logger.warning("ErrorCode: " + error_code)
+                return redirect(url_for('index'))
+            with open(filename_collector.filenames[0], "rb") as song_dong:
+                dong_data = song_dong.read(1024)
+                while dong_data:
+                    yield dong_data
+                    dong_data = song_dong.read(1024)
+
     r = request
     data = r.form
     url = data['url']
-    yt = YouTube(url)
-    stream = yt.streams.filter(only_audio=True).filter(file_extension='mp4').first()
-    stream.download('./mp4/')
-    return redirect(url_for('index'))
+    filename_collector = FilenameCollectorPP()
+    ydl_opts = {
+        'format': 'm4a/bestaudio/best',
+        'postprocessors': [{  # Extract audio using ffmpeg
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            }],
+        'outtmpl': {'default': './mp4/%(title)s [%(id)s].%(ext)s'},
+    }
+    filename = getfilenamefromurl(url)
+    return Response(generate(), mimetype="audio/mp3", headers={'Content-Disposition': 'attachment; filename = "' + filename + '"'})
+
+    # return redirect(url_for('index'))
+
+
+def getfilenamefromurl(url):
+    opts = {
+        'outtmpl': {'default': '%(title)s [%(id)s]'}
+    }
+    with yt_dlp.YoutubeDL(opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        filename = ydl.prepare_filename(ydl.sanitize_info(info)) + '.mp3'
+        return filename
 
 
 # if __name__ == "__main__":
